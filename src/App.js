a5828b2794f638c7f62a8332e715ffabc0d4e41c
Thomas Fouquert <tfouquert@gmail.com>
2022-08-29 17:49:33 +0200

    useContext

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import React, {Component} from 'react';
 import Todo from './component/Todo';
 import FunctionCount from './component/FunctionCount';
 import Container from './component/Container';
+import { UserContext, ColorContext } from './component/MyContext';
+import Profile from './component/Profile';
 
+class App extends Component {
 
+  state = {
+    user: {
+      name: "lisa",
+      age: 8
+    },
+    colorUser: 'red'
+  }
 
-function App() {
+  render () {
+
   return (
     <div className="App">
 
@@ -21,10 +33,19 @@ function App() {
 
 
 
+      <hr />
+      <UserContext.Provider value={this.state.user}>
+        <ColorContext.Provider value={this.state.colorUser}>
+          <Profile />
+        </ColorContext.Provider>
+      </UserContext.Provider>
+
+
+
 
 
     </div>
-  );
+  )}
 }
 
 export default App;
---
+++ src/component/MyContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserContext = React.createContext();
+
+const ColorContext = React.createContext();
+
+export {UserContext, ColorContext};
