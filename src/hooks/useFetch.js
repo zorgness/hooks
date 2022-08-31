1bb743548b901d4a6902aa05d2f70c287e4e5f74
Thomas Fouquert <tfouquert@gmail.com>
2022-08-31 19:07:55 +0200

    useFetchAsync

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -12,23 +12,52 @@ const useFetch = (url) => {
    useDebugValue(data, data => JSON.stringify(data));
 
 
+   const fetchData = async () => {
+
+      try {
+
+        const response = await fetch(url);
+        if(!response.ok) {
+          throw new Error('no response from server');
+        }
+
+        const fetchedData = await response.json();
+        console.log(fetchedData);
+        setData(fetchedData);
+        setInterval(() => {
+              setIsLoading(false);
+        }, 2000);
+       return () => clearInterval();
+
+      } catch (error) {
+        console.log(error.message);
+      }
+
+   }
+
 
    useEffect(() => {
 
+    fetchData();
+
+   }, [url]);
+
+  //  useEffect(() => {
 
 
-    fetch(url)
-    .then(response => response.json())
-    .then(json => {
-      setData(json);
-      setInterval(() => {
-        setIsLoading(false);
-      }, 2000);
-      return () => clearInterval();
 
-    })
-    .catch(err => { console.log(err.message); });
-  }, [url]);
+  //   fetch(url)
+  //   .then(response => response.json())
+  //   .then(json => {
+  //     setData(json);
+  //     setInterval(() => {
+  //       setIsLoading(false);
+  //     }, 2000);
+  //     return () => clearInterval();
+
+  //   })
+  //   .catch(err => { console.log(err.message); });
+  // }, [url]);
 
   return {data, isLoading}
 
